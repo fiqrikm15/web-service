1df92e6bb13826bf4a94c5a736d2ac8afce1d5fd
fiqri khoirul m <fiqrikm15@gmail.com>
2017-10-07 17:43:18 +0700

    POST method REST full

--- application/controllers/Book.php
+++ application/controllers/Book.php
@@ -16,7 +16,25 @@ class Book Extends REST_Controller{
 
 	// mengirim data
 	function index_post(){
+		$isbn = $this->post('isbn');
+		$title = $this->post('title');
+		$writer = $this->post('writer');
+		$description = $this->post('description');
 
+		$book = array('title' => $title,
+				'isbn'=> $isbn,
+				'writer' => $writer,
+				'description' => $description
+			);
+
+		$insert = $this->db->insert('book', $book);
+
+		if($insert)
+			$this->response($book, 200);
+		else{
+			$data = array('status', 'Gagal insert');
+			$this->response($data, 502);
+		}
 	}
 
 }
